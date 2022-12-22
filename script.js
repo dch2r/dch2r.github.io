52612f7a9a255351eb62acc269955b36e337312c
Diwesh Chaurasia <110768034+dch2r@users.noreply.github.com>
2022-12-23 00:53:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ========================================== typing animation =========================================== */
+var typed = new Typed(".typing",{
+    strings:["","Computer Engineer", "Web Designer", "Tech Enthusiast"],
+    typeSpeed: 100,
+    BackSpeed:60,
+    loop:true
+})
